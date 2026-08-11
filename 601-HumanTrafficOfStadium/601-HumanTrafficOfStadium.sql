f8a9132d341934830d1e3e66734677684e5b0612
Revanth-Reddy <mudireddyrevanthreddy63@gmail.com>
2026-08-11 14:17:36 +0530

    Create 601-HumanTrafficOfStadium

---
+++ 601-HumanTrafficOfStadium/601-HumanTrafficOfStadium.sql
@@ -0,0 +1,13 @@
+-- Last updated: 11/08/2026, 14:17:35
+SELECT id, visit_date, people
+FROM (
+    SELECT *,
+           LEAD(people,1) OVER (ORDER BY id) AS next1,
+           LEAD(people,2) OVER (ORDER BY id) AS next2,
+           LAG(people,1) OVER (ORDER BY id) AS prev1,
+           LAG(people,2) OVER (ORDER BY id) AS prev2
+    FROM Stadium
+) t
+WHERE (people >= 100 AND next1 >= 100 AND next2 >= 100)
+   OR (people >= 100 AND prev1 >= 100 AND prev2 >= 100)
+   OR (people >= 100 AND prev1 >= 100 AND next1 >= 100);
