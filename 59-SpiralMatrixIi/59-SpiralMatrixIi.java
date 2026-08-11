a8aec6e94709e0a614e87a7662208a358fe1b666
Revanth-Reddy <mudireddyrevanthreddy63@gmail.com>
2026-08-11 14:20:28 +0530

    Create 59-SpiralMatrixIi

---
+++ 59-SpiralMatrixIi/59-SpiralMatrixIi.java
@@ -0,0 +1,28 @@
+// Last updated: 11/08/2026, 14:20:27
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) matrix[top][j] = num++;
+            top++;
+
+            for (int i = top; i <= bottom; i++) matrix[i][right] = num++;
+            right--;
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) matrix[bottom][j] = num++;
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) matrix[i][left] = num++;
+                left++;
+            }
+        }
+        return matrix;
+    }
+}
