072ff9586e3c897e5f7532c199cc84e84c32a8ae
Revanth-Reddy <mudireddyrevanthreddy63@gmail.com>
2026-08-11 14:15:39 +0530

    Create 1480-MovieRating

---
+++ 1480-MovieRating/1480-MovieRating.sql
@@ -0,0 +1,19 @@
+-- Last updated: 11/08/2026, 14:15:38
+(
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr ON u.user_id = mr.user_id
+    GROUP BY u.user_id
+    ORDER BY COUNT(mr.rating) DESC, u.name
+    LIMIT 1
+)
+UNION ALL
+(
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr ON m.movie_id = mr.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.movie_id
+    ORDER BY AVG(mr.rating) DESC, m.title
+    LIMIT 1
+);
